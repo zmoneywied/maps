4e7658dc674efcca5c3b865c7c729a40d8d39f34
zmoneywied <zacharywiedeback@gmail.com>
2026-07-15 19:21:14 -0600

    Add files via upload

---
+++ qgis2web_2026_07_15-19_19_59_438440/styles/ClinicswithSpecialtyWomensHealthServices_2_style.js
@@ -0,0 +1,44 @@
+var size = 0;
+var placement = 'point';
+
+var style_ClinicswithSpecialtyWomensHealthServices_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("womens health — split_fid");
+    var labelFont = "9.1px \'Arial\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "#000000";
+    var bufferWidth = 3.0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("field_1") !== null) {
+        labelText = String(feature.get("field_1"));
+    }
+    if (value >= 0.000000 && value <= 0.100000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 0.100000 && value <= 145.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    };
+
+    return style;
+};
