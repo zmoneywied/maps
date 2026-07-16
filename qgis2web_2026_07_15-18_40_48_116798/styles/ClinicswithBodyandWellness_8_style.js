353d13ae9ea3efc13e3303aefc37b5ba2233a714
zmoneywied <zacharywiedeback@gmail.com>
2026-07-15 19:05:26 -0600

    Add files via upload

---
+++ qgis2web_2026_07_15-18_40_48_116798/styles/ClinicswithBodyandWellness_8_style.js
@@ -0,0 +1,56 @@
+var size = 0;
+var placement = 'point';
+
+var style_ClinicswithBodyandWellness_8 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("body & wellness — split_fid");
+    var labelFont = "9.1px \'Arial\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "#000000";
+    var bufferWidth = 3.0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("field_1") !== null) {
+        labelText = String(feature.get("field_1"));
+    }
+    if (value >= 0.000000 && value <= 0.100000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 11.0 + size, points: 5,
+            radius2: 5.5, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 12.0 + size, points: 5,
+            radius2: 6.0, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 0.100000 && value <= 143.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 11.0 + size, points: 5,
+            radius2: 5.5, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 12.0 + size, points: 5,
+            radius2: 6.0, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    };
+
+    return style;
+};
---
+++ qgis2web_2026_07_15-18_40_48_116798/styles/Colorado_ZIP_Code_Tabulation_Areas_ZCTAshp_1_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_Colorado_ZIP_Code_Tabulation_Areas_ZCTAshp_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("ZCTA5CE10") !== null) {
+        labelText = String(feature.get("ZCTA5CE10"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(53,121,177,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.6479999999999997}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
